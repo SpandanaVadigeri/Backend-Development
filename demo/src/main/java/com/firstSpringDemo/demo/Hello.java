ecf88cbf19d5a1e11fc886e3a5f7882f9e3759a4
Spandana <spandanavadigeri@gmail.com>
2025-07-26 11:58:57 +0530

    Updated Hello.java

--- demo/src/main/java/com/firstSpringDemo/demo/Hello.java
+++ demo/src/main/java/com/firstSpringDemo/demo/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 
     @RequestMapping
     public String greet(){
-        return "Hello World!" ;
+        return "Hello World! Welcome to the world of Spring Boot" ;
     }
 }
 
